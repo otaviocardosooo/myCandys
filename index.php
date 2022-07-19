9d472598cddb68bf068812a53436e1b35eac7c38
otaviocardosooo <81239817+otaviocardosooo@users.noreply.github.com>
2022-07-19 16:24:24 -0300

    chore(*): first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Importando Head e Header
+include_once  './includes/_head.php';
+include_once './includes/_header.php';
+?>
+
+<main class="mainIndex">
+    <div class="start">
+        <section class="header">
+            <nav>
+                <ul>
+                    <li><a>Home</a></li>
+                    <li><a>Produtos</a></li>
+                    <img src="./assets/images/umbrella.png" alt="Logo" id="logo">
+                    <li><a>Sobre Nós</a></li>
+                    <li><a>Contato</a></li>
+                </ul>
+            </nav>
+        </section>
+    </div>
+</main>
+</body>
+<?php
+// Importando Footer
+include_once './includes/_footer.php';
+?>
