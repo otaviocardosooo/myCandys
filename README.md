459e130b40f03bb762d49411b3ee1f18d48d2d61
otaviocardosooo <81239817+otaviocardosooo@users.noreply.github.com>
2022-07-21 16:45:38 -0300

    chore(*): index updates
    Primeiro commit adicionando a parte "start"

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# myCandys
+# myCandy's
+
+<img src="" alt="Imagem do Site">
+
+> E-Commerce desenvolvido como tarefa do técnico. 
+
+# Design 📚
+
+<a href="https://www.figma.com/file/ZpdX5HCOkcB2InI7SrndeM/Studies---myCandy's?node-id=0%3A1"><img src="https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white"></a>
+
+# Ajustes e Melhorias 📁
+
+Segue as tarefas e atualizações necessárias: 
+
+- [] Finalizar o Design
+- [] Finalizar o código  
  
+# Desenvolvedores 💻   
+
+<table>
+    <tr>
+        <td align="center">
+            <a href="https://github.com/ma1on3se">
+                <img src="https://avatars.githubusercontent.com/u/81239817?v=4" width="125px;" alt="Foto do Otávio Cardoso no Github"/><br>
+                <sub>
+                <b style="font-size: 13px;">Otávio Cardoso</b>
+                </sub>
+            </a>
+        </td>
--- index.php
+++ index.php
@@ -5,16 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myCandys</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/styles/index.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
-<body>
-<header>
-
-</header>
-<main class="mainIndex">
+<body>     
     <div class="start">
-        <section class="header">
+        <header class="nav justify-content-center">
             <nav>
                 <ul>
                     <li><a>Home</a></li>
@@ -24,12 +20,19 @@
                     <li><a>Contato</a></li>
                 </ul>
             </nav>
-        </section>
+        </header>
+        <div class="container title">
+                <h1>myCandy's</h1>
+                <h4>Melhor Qualidade, Melhor Sabor, Melhor Entrega</h4>
+        </div>
     </div>
+<main>
 </main>
 <footer>
 
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
